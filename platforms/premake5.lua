ad3a3315aa5b9525de6d33102e00b60553732da3
Olivier Chatry <olivier@xvision.no>
2016-01-28 06:34:44 +0100

    x build files are now generated in ../build folder
    easier to gitignore.

--- platforms/premake5.lua
+++ platforms/premake5.lua
@@ -1,26 +1,30 @@
 workspace "HolbertonWar"
   configurations {"Debug", "Release"}
+  floatingpoint "Fast"
+  flags       { "Symbols" }
+  location ("../build")
+
   project "vm"
+    debugdir "../cores"
     kind "ConsoleApp"
     language "C"
-    targetdir "bin/%{cfg.buildcfg}"
     files { "../vm/**.h", "../vm/**.c" }
     files { "../common/**.h", "../common/**.c" }
     filter "configurations:Debug"
-       defines { "DEBUG" }
-       flags { "Symbols" }
+      defines { "DEBUG" }
+      flags { "Symbols" }
     filter "configurations:Release"
-       defines { "NDEBUG" }
-       optimize "On"
-   project "asm"
-     kind "ConsoleApp"
-     language "C"
-     targetdir "bin/%{cfg.buildcfg}"
-     files { "../asm/**.h", "../asm/**.c" }
-     files { "../common/**.h", "../common/**.c" }
-     filter "configurations:Debug"
-        defines { "DEBUG" }
-        flags { "Symbols" }
-     filter "configurations:Release"
-        defines { "NDEBUG" }
-        optimize "On"
+      defines { "NDEBUG" }
+      optimize "On"
+  project "asm"
+    debugdir "../cores"
+    kind "ConsoleApp"
+    language "C"
+    files { "../asm/**.h", "../asm/**.c" }
+    files { "../common/**.h", "../common/**.c" }
+    filter "configurations:Debug"
+      defines { "DEBUG" }
+      flags { "Symbols" }
+    filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
