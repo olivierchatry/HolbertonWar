32ee97cfb6eaded3b8a9bcf319eff2e602f153a4
Olivier Chatry <olivier@fieldap.com>
2017-02-10 09:23:58 +0100

    linux : use bundled glfw library

--- platforms/premake5.lua
+++ platforms/premake5.lua
@@ -54,13 +54,7 @@ workspace "HolbertonWar"
 
 		filter "system:linux"
 			toolset "clang"
-			includedirs "../externals/glfw/include"
-			if os.is64bit() then
-				libdirs "../externals/glfw/lib/linux/x86_64"
-			else
-				libdirs "../externals/glfw/lib/linux/x86"
-			end
-			links {"glfw3", "X11", "Xxf86vm", "Xrandr", "Xi", "GL", "pthread", "m", "dl"}
+			links {"glfw", "X11", "Xxf86vm", "Xrandr", "Xi", "GL", "pthread", "m", "dl"}
 
 		filter "system:macosx"
 			platforms 	{"Universal"}
